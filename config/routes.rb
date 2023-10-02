008b0c53cd536c142c41a0229c0bbe7d76b65426
Terrence Tyson <tsqware@gmail.com>
2023-10-02 16:48:18 -0400

    ch11 authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+	devise_for :users
+	
 	resources :blog_posts
 
   # Defines the root path route ("/")
